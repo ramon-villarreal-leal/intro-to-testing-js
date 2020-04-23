ff69babc2fb8a64b1af2970370e689b90da541fe
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-04-23 14:47:34 -0500

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -1,4 +1,18 @@
-// helloWorld function 
-function helloWorld() {
+// helloWorld function
+//same as function helloWorld() { } because functions are first class citizens.
+const helloWorld = function() {
     return "Hello, World!";
 }
+
+const sayHello = function() {
+    return true;
+}
+
+
+// function isNumeric () {
+//     if(input === 'Jim Henson') {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
--- tests.js
+++ tests.js
@@ -1,15 +1,38 @@
 // Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+describe('helloWorld', function () {
+    it('should be a defined function', function () {
+        expect(typeof helloWorld).toBe('function');
     });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+    it('should return a string when called', function () {
+        expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(helloWorld()).toBe("Hello, World!");
     });
-    it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+    it("should never return 'undefined' when called", function () {
+        expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe("sayHello", function () {
+    it('it should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
+    });
+});
+
+// describe("isNumeric", function () {
+//     it("should be a defined function", function () {
+//         expect(typeof isNumeric)
+//     ).toBe("function");
+// });
+// it("should return a boolean value when called", function () {
+//     expect(typeof isNumeric()).toBe("boolean");
+// });
+// it("should return true when passed the number 5", function () {
+//     expect(typeof isNumeric(5)).toBe("true");
+// });
+// it("should return false when passed the string 'Jim Henson", function () {
+//     expect(isNumeric("Jim Henson")).toBe(false);
+// });
+// })
+// ;
